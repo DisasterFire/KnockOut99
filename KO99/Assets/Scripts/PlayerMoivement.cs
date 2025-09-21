b9c0969e89fb18ed2d3159f2b00461772e1203d4
Nik <anipresser@gmail.com>
2025-09-21 19:45:00 -0400

    Animated Walking and punching

--- KO99/Assets/Scripts/PlayerMoivement.cs
+++ KO99/Assets/Scripts/PlayerMoivement.cs
@@ -6,14 +6,18 @@ public class PlayerMoivement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public Transform tf;
+    public Animator anim;
     public Vector3 InputKey;
     public float moveSpeed = 10;
+    public bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        anim = GetComponent<Animator>();
+        tf = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -21,5 +25,36 @@ public class PlayerMoivement : MonoBehaviour
     {
         InputKey = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         rb.velocity = InputKey * moveSpeed;
+
+        if (InputKey.x > 0 || InputKey.x < 0 || InputKey.z > 0 || InputKey.z < 0) { anim.SetBool("IsWalking", true); }
+        else { anim.SetBool("IsWalking", false); }
+
+        if ((InputKey.x > 0 || InputKey.z > 0) && !facingRight)
+        {
+            FlipSprite();
+        }
+        else if ((InputKey.x < 0 || InputKey.y < 0) && facingRight)
+        {
+            FlipSprite();
+        }
+
+        Punch();
+    }
+
+    void FlipSprite() 
+    {
+        // Rotate 180 degrees around the local y-axis
+        transform.Rotate(0f, 180f, 0f);
+
+        // Update the facing direction flag
+        facingRight = !facingRight;
+    }
+
+    void Punch()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.Play("Punch");
+        }
     }
 }
